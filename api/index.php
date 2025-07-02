bcbd1d552a333e644dd604bfd964ac93390027b0
Vinita More <vinita.more@feedspotmail.com>
2025-07-02 18:46:02 +0530

    2nd july 2025

--- api/index.php
+++ api/index.php
@@ -1,9 +1,23 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-$file = "../data.json";
-if (file_exists($file)) {
-    echo file_get_contents($file);
-} else {
-    echo json_encode(["error" => "File not found"]);
+
+require_once '../constant.php';
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name); 
+
+if ($conn->connect_error) {
+    die(json_encode(["error" => $conn->connect_error]));
 }
+
+$sql = "SELECT id, title, description, image, feedurl FROM dummyfeeddata";
+$result = $conn->query($sql);
+
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+$conn->close();
